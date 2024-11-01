9521c0ed4dff9a38a4be798d90e82c346511d382
deris.o2024 <deris.omalley@mymail.champlain.edu>
2024-11-01 18:14:57 -0400

    testFontSize??

--- gameScript.js
+++ gameScript.js
@@ -1,6 +1,23 @@
-let canvas = document.getElementById('myCanvas');
+// Canvas Variables
+let c = document.getElementById('myCanvas');
 let ctx = canvas.getContext('2d');
+
+// Start Page Variables
+
+// Choose Character Variables
+let chooseCharacterContainer = document.querySelector('.chooseCharacterContainer')
+
+let init = () => {
+    let pageNum = 0;
+
+    // Block Choose Character Page
+    chooseCharacterContainer.style.display = 'none';
+}
 
 //Possibly Add Classes
 
 //Text Based Game
+
+if(pageNum == 0){
+    
+}
